0eff7bb100dd43eb21db23dbbf29ef5cba3df130
Paolo Labs <paololabsplus@gmail.com>
2015-06-29 01:01:09 -0400

    My first commit

---
+++ MyFirstApplication/MyFirstApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApplication
+//
+//  Created by Paolo Jhonatan Ramos Méndez on 29/6/15.
+//  Copyright (c) 2015 Paolo J. Ramos Méndez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstApplication/MyFirstApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApplication
+//
+//  Created by Paolo Jhonatan Ramos Méndez on 29/6/15.
+//  Copyright (c) 2015 Paolo J. Ramos Méndez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
